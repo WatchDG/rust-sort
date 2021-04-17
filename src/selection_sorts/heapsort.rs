d9f17e72e6ff09eee3e230559363b44cec1f511d
WatchDG <baranov@w3m.co>
2021-04-17 16:00:02 +0300

    fix heapsort

--- src/selection_sorts/heapsort.rs
+++ src/selection_sorts/heapsort.rs
@@ -10,31 +10,33 @@ mod tests {
     }
 }
 
-fn heapify<T: PartialOrd>(v: &mut [T]) {
+fn _heapify<T: PartialOrd>(v: &mut [T], mut i: usize) {
     let l = v.len();
-    for i in (0..(l / 2)).rev() {
-        let mut j = i;
-        loop {
-            let mut parent_idx = j;
-            let left_child_idx = 2 * j + 1;
-            let right_child_idx = 2 * j + 2;
-            if left_child_idx < l && v[left_child_idx] < v[parent_idx] {
-                parent_idx = left_child_idx;
-            }
-            if right_child_idx < l && v[right_child_idx] < v[parent_idx] {
-                parent_idx = right_child_idx;
-            }
-            if j == parent_idx {
-                break;
-            }
-            v.swap(j, parent_idx);
-            j = parent_idx;
+    loop {
+        let mut parent_idx = i;
+        let left_child_idx = 2 * i + 1;
+        let right_child_idx = 2 * i + 2;
+        if left_child_idx < l && v[left_child_idx] < v[parent_idx] {
+            parent_idx = left_child_idx;
         }
+        if right_child_idx < l && v[right_child_idx] < v[parent_idx] {
+            parent_idx = right_child_idx;
+        }
+        if i == parent_idx {
+            break;
+        }
+        v.swap(i, parent_idx);
+        i = parent_idx;
     }
 }
 
 pub fn heapsort<T: PartialOrd>(v: &mut [T]) {
-    for i in 0..v.len() {
-        heapify(&mut v[i..]);
+    let l = v.len();
+    for i in (0..(l / 2)).rev() {
+        _heapify(v, i);
+    }
+
+    for i in 0..l {
+        _heapify(&mut v[i..], 0);
     }
 }
