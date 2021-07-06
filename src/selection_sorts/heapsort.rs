a91efbe342bdafb397be421adb70579192866185
Paul Graydon <paulgraydon@hotmail.com>
2021-07-05 17:30:44 -1000

    Fix heapsort algorithm
    I found a scenario where the heap was incorrectly sorted, which I've
    added as a test case.
    
    I ended up just re-creating heapsort using the pseudocode on Wikipedia
    as my guide.

--- src/selection_sorts/heapsort.rs
+++ src/selection_sorts/heapsort.rs
@@ -3,40 +3,52 @@ mod tests {
     use crate::heapsort;
 
     #[test]
-    fn sort() {
+    fn sort_1() {
         let mut data = vec![5, 4, 3, 2, 1];
         heapsort(&mut data);
         assert_eq!(data, [1, 2, 3, 4, 5]);
     }
+
+    #[test]
+    fn sort_2() {
+        let mut data = vec![8, 7, 6, 5, 4, 6, 6, 3, 2, 1, 0];
+        let expected = vec![0, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8];
+
+        heapsort(&mut data);
+        assert_eq!(data, expected);
+    }
 }
 
-fn _heapify<T: PartialOrd>(v: &mut [T], mut i: usize) {
-    let l = v.len();
+fn _heapify<T: PartialOrd>(v: &mut [T], start: usize, end: usize) {
+    let mut root = start;
     loop {
-        let mut parent_idx = i;
-        let left_child_idx = 2 * i + 1;
-        let right_child_idx = 2 * i + 2;
-        if left_child_idx < l && v[left_child_idx] < v[parent_idx] {
-            parent_idx = left_child_idx;
+        let mut child = root * 2 + 1;
+
+        if child > end {
+            break;
+        };
+
+        if child + 1 <= end && v[child] < v[child + 1] {
+            child += 1;
         }
-        if right_child_idx < l && v[right_child_idx] < v[parent_idx] {
-            parent_idx = right_child_idx;
-        }
-        if i == parent_idx {
+
+        if v[root] < v[child] {
+            v.swap(root, child);
+            root = child;
+        } else {
             break;
         }
-        v.swap(i, parent_idx);
-        i = parent_idx;
     }
 }
 
 pub fn heapsort<T: PartialOrd>(v: &mut [T]) {
-    let l = v.len();
-    for i in (0..(l / 2)).rev() {
-        _heapify(v, i);
+    let length = v.len();
+    for start in (0..((length - 2) / 2) + 1).rev() {
+        _heapify(v, start, length - 1);
     }
 
-    for i in 0..l {
-        _heapify(&mut v[i..], 0);
+    for end in (1..length).rev() {
+        v.swap(end, 0);
+        _heapify(v, 0, end - 1);
     }
 }
